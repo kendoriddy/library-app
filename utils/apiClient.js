a4b41850d8c9748d95eee1799982d46893a9ac7f
ayanjuwon <oiayanniran@student.lautech.edu.ng>
2025-03-05 11:32:20 +0100

    removed books id folder from card collection

--- utils/apiClient.js
+++ utils/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXTAUTH_URL || "http://localhost:8004";
+const API_BASE_URL = process.env.NEXTAUTH_URL || "https://uilibraryservice.com.ng";
 
 export const apiClient = async (endpoint, options = {}) => {
   const url = `${API_BASE_URL}/api${endpoint}`;
